67ad1cdcf3902adf60add7ec7df1eacc2e9b57f6
David Chirgwin <dchirgwin@bluechiptechnology.co.uk>
2013-07-24 15:51:15 +0100

    BCT Set DVFS working points for improved operation
    This allows CPU voltage to be varied by DVFS for lower power consumption.

--- arch/arm/mach-mx6/cpu_op-mx6.c
+++ arch/arm/mach-mx6/cpu_op-mx6.c
@@ -97,7 +97,7 @@ static struct cpu_op mx6q_cpu_op_1G[] = {
 	 .pu_voltage = 1250000,
 	 .soc_voltage = 1250000,
 #endif
-	 .cpu_voltage = 1250000,},
+	 .cpu_voltage = 1150000,},
 #ifdef CONFIG_MX6_VPU_352M
 	/*pll2_pfd_400M will be fix on 352M,to avoid modify other code
 	which assume ARM clock sourcing from pll2_pfd_400M, change cpu
@@ -108,7 +108,7 @@ static struct cpu_op mx6q_cpu_op_1G[] = {
 	  .cpu_podf = 0,
 	  .pu_voltage = 1250000,
 	  .soc_voltage = 1250000,
-	  .cpu_voltage = 950000,},
+	  .cpu_voltage = 1050000,},
 #else
 	 {
 	  .pll_rate = 396000000,
@@ -116,7 +116,7 @@ static struct cpu_op mx6q_cpu_op_1G[] = {
 	  .cpu_podf = 0,
 	  .pu_voltage = 1250000,
 	  .soc_voltage = 1250000,
-	  .cpu_voltage = 1250000,},
+	  .cpu_voltage = 1050000,},
 #endif
 };
 
